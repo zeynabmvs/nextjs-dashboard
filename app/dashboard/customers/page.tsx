990bbf56e271a1e495d99bc955b14b901ff49d15
Zeynab Movasaqpoor <30770159+zeynabmvs@users.noreply.github.com>
2024-06-26 09:51:39 -0700

    pt 4: layout & pages

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div>Customers page</div>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div>Invoices page</div>
+  )
+}
+
+export default page
