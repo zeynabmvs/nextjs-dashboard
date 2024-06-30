ec55c69e3b778a69570fdd66c05bf785676b0774
Zeynab Movasaqpoor <30770159+zeynabmvs@users.noreply.github.com>
2024-06-30 16:15:59 -0700

    pt 12: edit Invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -32,3 +32,26 @@ export async function createInvoice(formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData){
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    UPDATE invoices
+    SET customer_id=${customerId}, amount=${amountInCents}, status=${status}
+    WHERE id = ${id}
+  `
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
